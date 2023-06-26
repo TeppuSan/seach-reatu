81fb455c5527e496042c96fc36207990eaa924cb
TeppuSan <StarVampire.teppu@gmail.com>
2023-06-26 23:50:25 +0900

    仮完成

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect, useRef } from 'react';
 
 function App() {
+  const[users, setUsers] = useState([]);
+  const[search, setSearch] = useState([]);
+  const ref = useRef();
+
+  const handleSearch = () => {
+    console.log(ref.current.value);
+
+    setSearch(
+    users.filter
+    ((user) => user.name.toLowerCase()
+    .includes(ref.current.value))
+    )
+  };
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then((res) => {
+    return res.json();
+  })
+  .then((data) => setUsers(data));
+  }, []);
+
+  console.log(users);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='main'>
+        <h2></h2>
+        <input type="text" ref={ref} onChange={() => handleSearch()}/>
+
+          <div className='content'>
+            {search.map((user) => (
+            <div className='box' key={user.id}>
+              <h3>{user.name}</h3>
+              <hr/>
+              <p>{user.email}</p>
+            </div>
+
+
+            ))}
+          </div>
+        </div>
+      </div>
   );
 }
 
